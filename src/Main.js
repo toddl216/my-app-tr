b4470aa53952ae56bdff1e7ed20cc259f5aadefa
toddl216 <21116@sshs.hs.kr>
2021-12-01 09:05:29 +0900

    commit

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,61 @@
 import React, { useRef } from 'react'
-import "./styles.css";
 import styled from "styled-components";
 //import './App.css';
+//import { useNavigate } from 'react-router-dom'
+//import { useDispatch, useSelector } from 'react-redux'
+//import { createUserName } from "../redux/modules/user";
 import cover from "./img/img.png";
 
 const Main = () => {
-
+  /*
+  const Navigate = useNavigate()
+  const dispatch = useDispatch()
+  */
   const nameInput = useRef('')
   const answerInput = useRef('')
+
+  setTimeout(function() {
+    alert('힌트 확인하세요 ㅇㅁㅇ');
+  }, 30000);
+
+  /*
+  useEffect(() => {
+    nameInput.current.focus()
+  }, [])
+
+  const checkExistName = () => {
+    return users.some(user => user.name === nameInput.current.value)
+  }
+  */
 
   // 입력 여부 확인 후 퀴즈 시작
   const quizStart = () => {
     const name = nameInput.current.value
     const answer = answerInput.current.value
-
     if (!name) {
       alert('교번을 입력해주세요!')
       nameInput.current.focus()
       return
     }
+
     if (!answer) {
       alert('답을 입력해주세요!')
       answerInput.current.focus()
       return
     }
+    /*
+    if (checkExistName()) {
+      alert('이미 사용된 이름이에요! 다른 이름을 입력해주세요 :)')
+      nameInput.current.focus()
+      return
+    }
+
+    dispatch(createUserName(name, false))
+    history.push('/quiz/1')
+    }
+    */
   }
-    
-  // 시작 버튼 클릭 동작
+  // 버튼 클릭 동작
   const handleClickNextBtn = () => {
     quizStart()
   }
@@ -46,13 +75,12 @@ const Main = () => {
     maxWidth: '400px', 
     margin: '30px auto', 
     flexDirection: 'column'
-  };
-
+  };  
 
   return (
     <div className="App">
       <div style={styles}>
-        <h1 style={{ color: 'orange' }}>꺄 정보부 데굴데굴 ㅇㅁㅇ</h1>
+        <h1 style={{ color: 'orange' }}>꺄 데굴데굴 정보부 ㅇㅁㅇ</h1>
         <hr style={{width: '100%'}}/>
         <StyledImage src={cover} alt="cover"></StyledImage>
         <Container>
@@ -69,11 +97,28 @@ const Main = () => {
           />
           <Button onClick={handleClickNextBtn}>제출!</Button>
         </Container>
+        <Explain>
+        <b>힌트</b> 입니다!<br/>
+        <b>일단 구현해볼게요!</b>
+      </Explain>
       </div>
     </div>
   );
 }
 
+const Explain = styled.p`
+  font-family: 'sans-serif';
+  font-size: 14px;
+  line-height: 1.72;
+  text-align: center;
+  padding: 16px;
+  border: 1px solid #ddd;
+  background-color: #f8f8f8;
+
+  b {
+    font-size: 18px;
+  }
+`
 
 const Button = styled.div`
   font-size: 18px;
@@ -100,7 +145,7 @@ const StyledImage = styled.img`
 	border-radius: 16px;
 	opacity: 100%;
 	margin-bottom: 1px;
-	max-width: 200%;
+	max-width: 100%;
 	display: block;
 `
 
